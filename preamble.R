fc3906097f6f9964b0fba4bf189ec9f0393434c0
Dylan Childs <d.childs@sheffield.ac.uk>
2017-03-19 18:08:40 +0000

    more ggplot2 stuff

--- preamble.R
+++ preamble.R
@@ -1,6 +1,12 @@
 library(dplyr)
 library(ggplot2)
+library(nasaweather)
 library(magrittr)
+library(knitr)
+
+# knitr options
+
+opts_chunk$set(fig.width=3.5, fig.height=3.5, fig.align="center")
 
 # Set plotting to bw plot default, but with transparent background elements. Note
 # transparency requires the panel.background, plot.background, and device background
